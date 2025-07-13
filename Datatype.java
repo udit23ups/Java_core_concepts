889aa278e356e90217f1ad7579565caa69c3164f
Udit Pratap Singh <152814569+udit23ups@users.noreply.github.com>
2025-07-13 16:33:26 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,30 @@
+public class Datatype {
+    public static void main(String[] args) {
+        //Integer type
+        int num=123;
+        long l=789978955L;
+
+        //Float type
+        float fnum=95.7f;
+        double dnum=4507.25;
+        
+        //character type
+        char ch='k';
+        
+        //Boolean types
+        Boolean bnum=true;
+
+        System.out.println("int num :"+num);
+        System.out.println("float num is:"+fnum);
+        System.out.println("double num is:"+dnum);
+        System.out.println("boolean num is:"+bnum);
+        System.out.println("long num is:"+l);
+        System.out.println("char is:"+ch);
+
+        
+
+    }
+
+    
+    
+} 
