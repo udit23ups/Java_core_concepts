5bb1f98b5c057d5f90d4416b9a68db79a87c91bf
Udit Pratap Singh <152814569+udit23ups@users.noreply.github.com>
2025-07-14 23:16:11 +0530

    Add files via upload

---
+++ literals.java
@@ -0,0 +1,21 @@
+public class literals {
+    public static void main(String args[]){
+       //binary
+        int num1=0b01;
+
+        //hexadecimal
+        int num2=0x54d;
+
+        //octal num
+        int num3=045;
+
+        // easy for counting digit as ( _ ) will does not effect anything
+        int num4=10_000_000;
+
+
+        System.out.println("binary to decimal:"+num1+"\nhex to decimal:"+num2+"\noctal to decimal:"+" "+num3+" \n"+num4);
+
+
+    }
+    
+}
